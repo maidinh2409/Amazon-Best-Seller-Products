c5fb785f44020b125fb65415479ebe94fd2b3721
maidinh2409 <134086665+maidinh2409@users.noreply.github.com>
2025-01-16 18:43:23 -0800

    Update products.py

--- spiders/products.py
+++ spiders/products.py
@@ -7,4 +7,7 @@ class ProductsSpider(scrapy.Spider):
     start_urls = ["https://www.amazon.ca/Best-Sellers-generic/zgbs/?ref_=nav_cs_bestsellers"]
 
     def parse(self, response):
+        category = response.css('div._p13n-zg-nav-tree-all_style_zg-browse-group__88fbz a::attr(href)').getall()
+        product_link = response.css('div.p13n-sc-uncoverable-faceout > a::attr(href)').getall()
+
         pass
